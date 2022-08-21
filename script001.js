d844338e54720b4330722bd55ac001259d509265
risarisato <nqg44226@gmail.com>
2022-08-21 18:15:22 +0900

    branchしてtest001で斜め線を確認

--- script001.js
+++ script001.js
@@ -3,27 +3,50 @@ window.addEventListener('load', function () {
   const canvas = document.getElementById('canvas001');
   const ctx = canvas.getContext('2d');
   canvas.width = 400;
-  canvas.height = 400;
+  canvas.height = 800;
 
   // プレイヤー
   class Player {
     constructor(game) {
       this.game = game;
       // プレイヤーの大きさ
-      this.width = 120;
-      this.height = 190;
+      this.width = 12;
+      this.height = 19;
       // プレイヤー位置
       this.x = 20;
       this.y = 100;
-      this.speedY = 1;
+      this.speedY = 0.3;
+      this.speedX = 0.3;
+    }
+    update() {
+      this.y += this.speedY;
+      this.x += this.speedX;
+    }
+    // context引数を外からもってくるやりかた＝ctx同じ
+    draw(context) {
+      context.fillStyle = 'red';
+      context.fillRect(this.x, this.y, this.width, this.height); //塗りつぶし
+    }
+  }
+
+  class Player2 {
+    constructor(game) {
+      this.game = game;
+      // プレイヤーの大きさ
+      this.width = 120;
+      this.height = 190;
+      // プレイヤー位置
+      this.x = 200;
+      this.y = 50;
+      this.speedY = 0.3;
     }
     update() {
       this.y += this.speedY;
     }
     // context引数を外からもってくるやりかた＝ctx同じ
     draw(context) {
-      //context.fillStyle = 'black';//プレイヤーの色→strokeRectでいらない
-      context.fillRect(this.x, this.y, this.width, this.height); 塗りつぶし
+      context.fillStyle = 'blue';
+      context.fillRect(this.x, this.y, this.width, this.height); //塗りつぶし
     }
   }
 
@@ -33,12 +56,15 @@ window.addEventListener('load', function () {
       this.width = width;
       this.height = height;
       this.player = new Player(this);
+      this.player2 = new Player2(this);
     }
     update() {
       this.player.update();
+      this.player2.update();
     }
     draw(context) {
       this.player.draw(context);
+      this.player2.draw(context);
     }
   }
 
